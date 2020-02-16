59915fdf37b75d58124dcbfaa553b45b84c81709
ismail5g <ismail27.dec@gmail.com>
2020-02-16 18:33:22 +0600

    Normally CRUD Operation Done

--- BookListRazorCRUD/Model/Book.cs
+++ BookListRazorCRUD/Model/Book.cs
@@ -14,5 +14,6 @@ namespace BookListRazorCRUD.Model
         [Required]
         public string Name { get; set; }
         public string Author { get; set; }
+        public string ISBN { get; set; }
     }
 }
