a8d1238d49abc7378e7beeed99acefe0ff3d077e
ismail5g <ismail27.dec@gmail.com>
2020-02-17 18:43:58 +0600

    Sweet Alert Used

--- BookListRazorCRUD/Startup.cs
+++ BookListRazorCRUD/Startup.cs
@@ -27,6 +27,7 @@ namespace BookListRazorCRUD
         {
             services.AddDbContext<ApplicationDbContext>(option=> option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddRazorPages().AddRazorRuntimeCompilation();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +53,7 @@ namespace BookListRazorCRUD
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapRazorPages();
             });
         }
